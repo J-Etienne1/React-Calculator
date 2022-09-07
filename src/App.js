30508f7e4a2a0def4568ca52baf95b0087ba01db
Jason <jasonetienne1@gmail.com>
2022-09-07 20:25:42 +0100

    added css to syle the calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import "./index.css"
+
 
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">1,234,1546</div>
+        <div className="current-operand">1,234,1546</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
